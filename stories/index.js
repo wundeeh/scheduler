ffd63c9a6a5da48b979953f5fe13a8e4a0186387
wundeeh <jakiriksh@gmail.com>
2023-01-10 14:29:17 -0600

    implement starter code

--- stories/index.js
+++ stories/index.js
@@ -23,3 +23,14 @@ storiesOf("Button", module)
       Disabled
     </Button>
   ));
+
+  storiesOf("DayListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => <DayListItem name="Monday" spots={5} />) // To define our stories, we call add() once for each of our test states to generate a story
+  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
+  .add("Full", () => <DayListItem name="Monday" spots={0} />)
+  .add("Clickable", () => (
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
+  ));

51905b05383384aaebff73527d91d45d2d72ef60
wundeeh <jakiriksh@gmail.com>
2023-01-10 14:49:06 -0600

    display of spots remaining is different when theres 1 or 0 left

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,6 +5,16 @@ import "components/DayListItem.scss"
 
 export default function DayListItem(props) {
 
+  const formatSpots = function() {
+    if (props.spots === 1) {
+      return ("1 spot remaining")
+    } else if (props.spots === 0) {
+      return ("no spots remaining")
+    } else {
+      return `${props.spots} spots remaining`
+    }
+  }
+
   let dayClass = classNames("day-list__item", {
     " day-list__item--selected": props.selected,
     " day-list__item--full": props.spots === 0
@@ -15,7 +25,7 @@ export default function DayListItem(props) {
     className={dayClass}
     onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }

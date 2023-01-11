f1babfce010e3a6475dc855055879a69b18a74f5
wundeeh <jakiriksh@gmail.com>
2023-01-11 17:43:34 -0600

    if no interview booked show Empty, else Show

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
+
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 import "components/Appointment/styles.scss"
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show /> : <Empty />}
+    </article>
   );
 }

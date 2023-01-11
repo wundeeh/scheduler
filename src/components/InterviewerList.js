fa36aba407dc9379fe5163a285b6fb0571cd5075
wundeeh <jakiriksh@gmail.com>
2023-01-11 14:39:23 -0600

    add status display for interviewers

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,26 @@
 import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
 
-import "components/InterviewerList.scss"
+import "components/InterviewerList.scss";
 
-export default function InterviewerList() {
+export default function InterviewerList(props) {
+
+  const interviewersArr = props.interviewers;
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        {interviewersArr.map((interviewer) => 
+          <InterviewerListItem 
+          key={interviewer.id}
+          name={interviewer.name}
+          avatar={interviewer.avatar}
+          selected={interviewer.id === props.interviewer}
+          setInterviewer={() => props.setInterviewer(interviewer.id)}
+          />
+        )}
+      </ul>
     </section>
   );
 }

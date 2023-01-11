fe52abc2d024b3a59974da21bcf67e37bc81466a
wundeeh <jakiriksh@gmail.com>
2023-01-11 15:37:20 -0600

    compatability with Empty function

--- stories/index.js
+++ stories/index.js
@@ -8,6 +8,7 @@ import InterviewerList from "components/InterviewerList";
 
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -137,4 +138,5 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", () => <Header time="12pm" />);
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>);

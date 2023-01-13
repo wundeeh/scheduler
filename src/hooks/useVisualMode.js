c6b0d6a24bb612bc18ac8b8c59e2b877f96567a0
wundeeh <jakiriksh@gmail.com>
2023-01-13 17:41:06 -0600

    add hook to go back and forth with the transitions

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,3 +1,25 @@
-import { renderHook, act } from "@testing-library/react-hooks";
+import { useState } from "react";
 
-import useVisualMode from "hooks/useVisualMode";
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    if (replace === true) {
+      setMode(newMode);
+      setHistory((prev) => [...prev]);
+    } else {
+      setMode(newMode);
+      setHistory((prev) => [...prev, newMode]);
+    }
+  }
+
+  function back() {
+    if (history.length !== 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
+    }
+  }
+
+  return { mode, transition, back };
+}

b3e4136e5b5dc834f329342f756d973a04d80115
wundeeh <jakiriksh@gmail.com>
2023-01-17 16:11:28 -0600

    minor spelling fix

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,10 +1,10 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from "prop-types";
 
 import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
-
   const interviewersArr = props.interviewers;
   const { value, onChange } = props;
 
@@ -12,16 +12,20 @@ export default function InterviewerList(props) {
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
-        {interviewersArr.map((interviewer) => 
-          <InterviewerListItem 
-          key={interviewer.id}
-          name={interviewer.name}
-          avatar={interviewer.avatar}
-          selected={interviewer.id === value}
-          setInterviewer={() => onChange(interviewer.id)}    
-        />
-        )}
+        {interviewersArr.map((interviewer) => (
+          <InterviewerListItem
+            key={interviewer.id}
+            name={interviewer.name}
+            avatar={interviewer.avatar}
+            selected={interviewer.id === value}
+            setInterviewer={() => onChange(interviewer.id)}
+          />
+        ))}
       </ul>
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};

3ed8eae71b791095d8530441a8c45d138ff38637
wundeeh <jakiriksh@gmail.com>
2023-01-17 18:49:00 -0600

    add a mock delete request

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -86,4 +86,11 @@ export default {
       statusText: "No Content",
     });
   }),
+
+  delete: jest.fn(() => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No content",
+    });
+  }),
 };
